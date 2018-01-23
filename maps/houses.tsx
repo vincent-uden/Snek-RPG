8c4ccba06ab1013274af94b8b427f9466133a31d
Vincent Uden <vincent.uden@itggot.se>
2018-01-23 22:19:06 +0100

    Added missing file

---
+++ maps/houses.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="houses" tilewidth="40" tileheight="40" tilecount="196" columns="14">
+ <image source="../resources/houses.png" width="560" height="560"/>
+</tileset>
